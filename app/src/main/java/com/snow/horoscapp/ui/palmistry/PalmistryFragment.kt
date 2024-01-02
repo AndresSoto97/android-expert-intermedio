cb6fc5ee122de3e3a2391e8d090665db1137a6ff
Snowcom415 <diegoasg5@gmail.com>
2024-01-02 15:47:14 -0600

    palmistrty

--- app/src/main/java/com/snow/horoscapp/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/snow/horoscapp/ui/palmistry/PalmistryFragment.kt
@@ -1,11 +1,16 @@
 package com.snow.horoscapp.ui.palmistry
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.core.content.PermissionChecker
 import androidx.fragment.app.Fragment
 import com.snow.horoscapp.databinding.FragmentPalmistryBinding
@@ -38,7 +43,19 @@ class PalmistryFragment : Fragment() {
     }
 
     private fun startCamera() {
-        TODO("Not yet implemented")
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+        cameraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+            val preview = Preview.Builder().build()
+                .also { it.setSurfaceProvider(binding.viewFinder.surfaceProvider) }
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+            try {
+                cameraProvider.unbindAll()
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+            } catch (e: Exception) {
+                Log.i("CAMERA ERROR", "Algo salio mal: ${e.message}")
+            }
+        }, ContextCompat.getMainExecutor(requireContext()))
     }
 
     private fun hasCameraPermission(): Boolean {
